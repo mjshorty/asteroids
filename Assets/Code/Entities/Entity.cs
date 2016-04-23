221646db9ad03bf4918470c2d7817e7b9e5d47fd
Michael Short <Michael Short>
2016-04-23 14:12:10 +0100

    Adding entity classes (player, enemy, asteroid etc)

---
+++ Assets/Code/Entities/Entity.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace entity
+{
+    public class Entity : MonoBehaviour
+    {
+        [SerializeField]
+        private float m_Dampening = 0.95f;
+
+        protected Vector3 m_Acceleration = Vector3.zero;
+        protected Vector3 m_Velocity = Vector3.zero;
+
+        virtual protected void UpdateEntity()
+        {
+        }
+
+        void Update()
+        {
+            UpdateEntity();
+
+            m_Velocity += m_Acceleration * Time.deltaTime;
+            transform.position += m_Velocity * Time.deltaTime;
+
+            m_Acceleration *= m_Dampening;
+            m_Velocity *= m_Dampening;
+
+            if (Vector3.SqrMagnitude(m_Velocity) < 0.001f * 0.001f)
+            {
+                m_Velocity = Vector3.zero;
+            }
+
+            if (Vector3.SqrMagnitude(m_Acceleration) < 0.001f * 0.001f)
+            {
+                m_Acceleration = Vector3.zero;
+            }
+        }
+    }
+}
---
+++ Assets/Code/Entities/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace entity
+{
+    public class Player : Entity
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        override protected void UpdateEntity()
+        {
+            UpdateInput();
+        }
+
+        private void UpdateInput()
+        {
+            if(Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                m_Acceleration += transform.forward.normalized * 10.0f;
+            }
+            else if(Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                transform.Rotate(0.0f, 0.0f, Time.deltaTime);
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                transform.Rotate(0.0f, 0.0f, -Time.deltaTime);
+            }
+        }
+    }
+}
