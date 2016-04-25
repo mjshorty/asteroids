d29ac747cdf20927449f2b011e7cfcea78e5cf0e
Michael Short <Michael Short>
2016-04-25 17:59:36 +0100

    Adding singleton for game object pool (reminder - use this for the spawners too)

---
+++ Assets/Code/Pooling/Singleton.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace utils
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        static public T Instance { get; private set; }
+
+        static public bool IsValid { get { return Instance != null} }
+
+        protected virtual void Awake()
+        {
+            if (!DestroyInstance())
+                CreateInstance();
+        }
+
+        protected virtual void OnEnable()
+        {
+#if UNITY_EDITOR
+            CreateInstance();
+#endif
+        }
+
+        protected virtual void OnDestroy()
+        {            
+            if (Instance == this)
+            {
+                Instance = null;
+            }
+        }
+
+        private void CreateInstance()
+        {
+            if (Instance == null)
+            {
+                Instance = this as T;
+            }
+        }
+
+        private bool DestroyInstance()
+        {
+            if (Instance != null)
+            {
+                Object.Destroy(gameObject);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
